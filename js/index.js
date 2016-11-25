47312b1d8ac61cd06ec3c85c9a747cefa1ba74b4
bassijtsma <btsijtsma@gmail.com>
2016-11-25 15:13:04 +0100

    work on functions

--- js/index.js
+++ js/index.js
@@ -14,30 +14,64 @@ require('aframe-animation-component');
 console.info('PRESS [CTRL] + [ALT] + I to start Aframe inspector');
 
 
+// //example component
+// AFRAME.registerComponent('toggle-for', {
+//     schema: {type: 'selector'},
+//     init: function() {
+//
+//         this.targetEntity = this.data;
+//         // console.info('init data', this.data);
+//
+//         this.el.addEventListener('click', evt => {
+//             // console.log(this.data);
+//             toggleBackground();
+//
+//             if( this.targetEntity.is('toggled')){
+//                 console.info('toggled off');
+//                 this.targetEntity.removeState('toggled');
+//                 this.targetEntity.emit('toggleoff');
+//                 this.targetEntity.emit('toggle');
+//             }else{
+//                 console.info('toggled on');
+//                 this.targetEntity.addState('toggled');
+//                 this.targetEntity.emit('toggleon');
+//                 this.targetEntity.emit('toggle');
+//             }
+//         });
+//     },
+//     tick: function(t,dt){
+//         //console.log(t,dt)
+//     },
+//     remove: function() {
+//     }
+// });
+
+
 //example component
-AFRAME.registerComponent('toggle-for', {
+AFRAME.registerComponent('backgroundswitch', {
     schema: {type: 'selector'},
     init: function() {
 
         this.targetEntity = this.data;
         // console.info('init data', this.data);
 
+        console.log('hier, this:');
+        console.log(this);
         this.el.addEventListener('click', evt => {
-            // console.log(this.data);
+            console.log(this.data);
             toggleBackground();
 
-            if( this.targetEntity.is('toggled')){
-                console.info('toggled off');
-                this.targetEntity.removeState('toggled');
-                this.targetEntity.emit('toggleoff');
-                this.targetEntity.emit('toggle');
-
-            }else{
-                console.info('toggled on');
-                this.targetEntity.addState('toggled');
-                this.targetEntity.emit('toggleon');
-                this.targetEntity.emit('toggle');
-            }
+            // if( this.targetEntity.is('toggled')){
+            //     console.info('toggled off');
+            //     this.targetEntity.removeState('toggled');
+            //     this.targetEntity.emit('toggleoff');
+            //     this.targetEntity.emit('toggle');
+            // }else{
+            //     console.info('toggled on');
+            //     this.targetEntity.addState('toggled');
+            //     this.targetEntity.emit('toggleon');
+            //     this.targetEntity.emit('toggle');
+            // }
         });
     },
     tick: function(t,dt){
@@ -47,10 +81,12 @@ AFRAME.registerComponent('toggle-for', {
     }
 });
 
+
+
 function toggleBackground() {
   var backgroundelement =  document.getElementById("background");
   var background = backgroundelement.getAttribute("src");
-  console.log("the background: " + background);
+  console.log("the bacXkground: " + background);
   if (background == "assets/background1.jpg" || background == "#background1") {
     backgroundelement.setAttribute("src", "assets/background2.jpg");
   } else {
